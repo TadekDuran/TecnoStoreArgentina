dc8f017eddf38ff8b6f1b5594056f34638fcd0f3
TadekDuran <tadekduran@gmail.com>
2025-02-14 15:08:05 -0300

    add: GET endpoint for single product

---
+++ src/app/api/products/getOne/route.jsx
@@ -0,0 +1,32 @@
+import { createClient } from '@/utils/supabase/server';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const id = searchParams.get('id');
+        if (!id) return NextResponse.json({ status: 400, message: "Null ID." }, { status: 400 });
+        const supabase = await createClient();
+        const { data, error } = await supabase.from("products").select().eq("id", id);
+        if (error) {
+            return NextResponse.json(
+                { status: 500, message: error.message },
+                { status: 500 }
+            );
+        }
+        return data.length > 0
+            ? NextResponse.json(data, { status: 200 })
+            : NextResponse.json(
+                {
+                    status: 404,
+                    message: `Product with ID ${id} not found.`,
+                },
+                { status: 404 }
+            );
+    } catch (error) {
+        return NextResponse.json(
+            { status: 500, message: error.message },
+            { status: 500 }
+        );
+    }
+}
