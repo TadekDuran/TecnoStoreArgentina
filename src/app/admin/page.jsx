56c5770d4f3616ee6f2142f3c424506d43c391b4
TadekDuran <tadekduran@gmail.com>
2025-02-18 13:33:01 -0300

    feature: render DataTable in Admin page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
+import { columns } from "./product/columns"
+import { DataTable } from "./product/data-table";
+import { useProducts } from "@/hooks";
 
 const Admin = () => {
-  return <div>Admin</div>;
-};
+  const { data, loading, error, getProducts } = useProducts();
+  useEffect(() => {
+    getProducts();
+  }, []);
+  
+  return (
+  <div suppressHydrationWarning > 
+    {loading && <p>Cargando productos...</p>}
+    {error && <p>Error al obtener productos: {error.message}</p>}
+    <DataTable columns={columns} data={data} />
+  </div>
+  );
+}
 
 export default Admin;
