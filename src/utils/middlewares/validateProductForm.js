dc351ad6fd38a7bde5602b6d5e5a77ba8bb601a7
TadekDuran <tadekduran@gmail.com>
2025-02-20 12:56:21 -0300

    feature: added conditions and error messages in validateProductForm

--- src/utils/middlewares/validateProductForm.js
+++ src/utils/middlewares/validateProductForm.js
@@ -1,25 +1,29 @@
 import { z } from "zod";
 
 export async function validateProductForm(formData) {
-    const formSchema = z.object({
-        category: z.string(),
-        model: z.string(),
-        maker: z.string(),
-        price: z.number(),
-        specs: z.array(
-            z.object({
-                name: z.string(),
-                value: z.string()
-            })
-        ),
-        featured: z.boolean(),
-        used: z.boolean(),
-        stock: z.boolean(),
-        colors: z.array(
-            z.object({
-                name: z.string(),
-                value: z.string()
-            })
-        ),
-    });
+  const formSchema = z.object({
+    category: z.string().min(1, "La categoría es obligatoria."),
+    model: z.string().min(1, "El modelo es obligatorio."),
+    maker: z.string().min(1, "El fabricante es obligatorio."),
+    price: z.number().positive("El precio debe ser un número positivo"),
+    specs: z
+      .array(
+        z.object({
+          name: z.string().min(1, "La especificación debe tener un nombre."),
+          value: z.string().min(1, "La especificación debe tener un valor."),
+        }),
+      )
+      .nonempty("Debe haber al menos una especificación"),
+    featured: z.boolean(),
+    used: z.boolean(),
+    stock: z.boolean(),
+    colors: z
+      .array(
+        z.object({
+          name: z.string().min(1, "El color debe tener un nombre."),
+          value: z.string().min(1, "El color debe tener un enlace."),
+        }),
+      )
+      .nonempty("Debe haber al menos un color."),
+  });
 }
