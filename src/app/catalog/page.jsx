8c515cda725b5d08d6e8a4fdeb083a3e5ecf5e48
TadekDuran <tadekduran@gmail.com>
2025-03-11 17:50:55 -0300

    (catalog): remove unnecessary queryParam handler, now default query has featured: true field. Also minor desing adjustements

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -1,37 +1,37 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
 import { useProducts } from "@/hooks";
 import ProductsFilter from "@/components/product/ProductsFilter";
+import { LoaderCircle } from 'lucide-react';
 
 const Catalog = () => {
   const { data, loading, error, getProducts } = useProducts();
-  const searchParams = useSearchParams();
-  const [queries, setQueries] = useState({});
+  const [queries, setQueries] = useState({ featured: true });
 
   useEffect(() => {
-    const category = searchParams.get("category");
-    setQueries({ category });
-  }, [searchParams]);
-
-  useEffect(() => {
-    if (Object.keys(queries).length > 0) {
-      getProducts(queries);
-    }
+    getProducts(queries);
   }, [queries]);
 
   return (
-    <div>
+    <div className="px-20 h-full">
       <p>Catálogo</p>
-      <ProductsFilter queries={queries} setQueries={setQueries} />
-      <div className="flex w-full justify-between gap-6 px-20">
-        {data.map((product) => (
-          <div className="flex flex-col" key={product.id}>
-            <p>{product.model}</p>
-            <p>{product.price}</p>
-            <p>{product.category}</p>
-          </div>
-        ))}
+      <div className="flex gap-10 justify-center">
+        <ProductsFilter queries={queries} setQueries={setQueries} />
+        <div className="flex w-4/5 justify-between gap-6 bg-slate-800 p-2">
+          {loading && <LoaderCircle className="animate-spin" />}
+          {error && <p>{error.message}</p>}
+          {!loading && !error && (
+            <>
+              {data.map((product) => (
+                <div className="flex flex-col" key={product.id}>
+                  <p>{product.model}</p>
+                  <p>{product.price}</p>
+                  <p>{product.category}</p>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
