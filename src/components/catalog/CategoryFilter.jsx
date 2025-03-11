027079b7b5d731cfd2950031807436a4d746d924
TadekDuran <tadekduran@gmail.com>
2025-03-11 17:44:07 -0300

    feature: create CategoryFilter accordion component

---
+++ src/components/catalog/CategoryFilter.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button } from '@/components/ui/button';
+
+const CategoryFilter = ({ handleChange }) => {
+
+	const categoryList = [
+		{ title: "Smartphones", category: "Smartphone" },
+		{ title: "Tablets", category: "Tablet" },
+		{ title: "Notebooks", category: "Notebook" },
+		{ title: "Consolas", category: "Consola" },
+	];
+
+	return (
+		<Accordion type="single" collapsible>
+			<AccordionItem value="item-1">
+				<AccordionTrigger>Categorías</AccordionTrigger>
+				{categoryList.map((category) => (
+					<AccordionContent key={category.title}>
+						<Button variant="ghost" onClick={() => handleChange("category", category.category)}>
+							{category.category}
+						</Button>
+					</AccordionContent>
+				))}
+			</AccordionItem>
+		</Accordion>
+	)
+}
+
+export default CategoryFilter
