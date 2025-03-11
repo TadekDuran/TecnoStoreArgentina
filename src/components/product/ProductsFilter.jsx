9b42ac629eb972cf8a88d8bfd790833f91d2e4f3
TadekDuran <tadekduran@gmail.com>
2025-03-11 17:48:27 -0300

    feature(ProductsFilter): implement CategoryFilter, now category filter replaces featured when added. Also minor design adjustments

--- src/components/product/ProductsFilter.jsx
+++ src/components/product/ProductsFilter.jsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/select"
 import { Checkbox } from "@/components/ui/checkbox"
 import { useDebounce } from "@/hooks/useDebounce";
+import CategoryFilter from "@/components/catalog/CategoryFilter";
 
 const ProductsFilter = ({ queries, setQueries }) => {
 
@@ -23,85 +24,99 @@ const ProductsFilter = ({ queries, setQueries }) => {
   }, [debouncedFilters, setQueries]);
 
   const handleChange = (field, value) => {
-    setLocalFilters((prev) => ({
-      ...prev,
-      [field]: field === "minPrice" || field === "maxPrice" ? parseInt(value) || "" : value,
-    }));
+    {
+      setLocalFilters((prev) => {
+        let updatedFilters = { ...prev, [field]: value };
+  
+        if (field === "category") {
+          delete updatedFilters.featured;
+        }
+  
+        return updatedFilters;
+      });
+    };
   };
 
-  const clearFilters = () =>  {
-    setQueries({});
-    setLocalFilters({})
+  const clearFilters = () => {
+    setLocalFilters((prev) => {
+      return prev.category ? { category: prev.category } : {};
+    });
   }
 
   return (
-    <div className='flex flex-col gap-4 p-2'>
+    <div className='w-1/5 flex flex-col gap-4'>
+      <CategoryFilter handleChange={handleChange} />
 
-      <div className='flex flex-col'>
-        <Label htmlFor="model">Modelo</Label>
-        <Input
-          type="text"
-          id="model"
-          value={localFilters.model || ""}
-          placeholder="Filtrar por modelo" onChange={(e) => handleChange("model", e.target.value)}
-        />
-      </div>
+      {localFilters.category && (
+        <>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="model">Modelo</Label>
+            <Input
+              type="text"
+              id="model"
+              value={localFilters.model || ""}
+              placeholder="Filtrar por modelo"
+              onChange={(e) => handleChange("model", e.target.value)}
+            />
+          </div>
 
-      <div className="flex flex-col">
-        <Label htmlFor="minPrice">Precio Mínimo</Label>
-        <Input
-          type="number"
-          id="minPrice"
-          value={localFilters.minPrice || ""}
-          placeholder="0"
-          onChange={(e) => handleChange("minPrice", e.target.value)}
-        />
-        <Label htmlFor="maxPrice">Precio Máximo</Label>
-        <Input
-          type="number"
-          id="maxPrice"
-          value={localFilters.maxPrice || ""}
-          placeholder="10000"
-          onChange={(e) => handleChange("maxPrice", e.target.value)}
-        />
-      </div>
+          <div className="flex gap-2">
+            <Label htmlFor="minPrice">Precio Mínimo</Label>
+            <Input
+              type="number"
+              id="minPrice"
+              value={localFilters.minPrice || ""}
+              placeholder="0"
+              onChange={(e) => handleChange("minPrice", e.target.value)}
+            />
+            <Label htmlFor="maxPrice">Precio Máximo</Label>
+            <Input
+              type="number"
+              id="maxPrice"
+              value={localFilters.maxPrice || ""}
+              placeholder="10000"
+              onChange={(e) => handleChange("maxPrice", e.target.value)}
+            />
+          </div>
 
-      <div className='flex flex-col'>
-        <Label htmlFor="maker">Fabricante</Label>
-        <Select
-          value={localFilters.maker || ""}
-          onValueChange={(value) => handleChange("maker", value)}
-        >
-          <SelectTrigger>
-            <SelectValue placeholder="Selecciona un fabricante" />
-          </SelectTrigger>
-          <SelectContent>
-            {makerList.map((maker, index) => (
-              <SelectItem key={index} value={maker}>
-                {maker}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="maker">Fabricante</Label>
+            <Select
+              value={localFilters.maker || ""}
+              onValueChange={(value) => handleChange("maker", value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Selecciona un fabricante" />
+              </SelectTrigger>
+              <SelectContent>
+                {makerList.map((maker, index) => (
+                  <SelectItem key={index} value={maker}>
+                    {maker}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
 
-      <div className="flex items-center gap-2">
-        <Checkbox
-          id="used"
-          checked={localFilters.used || false}
-          onCheckedChange={(value) => handleChange("used", value)}
-        />
-        <Label htmlFor="used">Usado</Label>
-      </div>
+          <div className="flex items-center gap-2">
+            <Checkbox
+              id="used"
+              checked={localFilters.used || false}
+              onCheckedChange={(value) => handleChange("used", value)}
+            />
+            <Label htmlFor="used">Usado</Label>
+          </div>
 
-      <div className="flex items-center gap-2">
-        <Checkbox
-          id="featured"
-          checked={localFilters.featured || false}
-          onCheckedChange={(value) => handleChange("featured", value)}
-        />
-        <Label htmlFor="featured">Destacado</Label>
-      </div>
+          <div className="flex items-center gap-2">
+            <Checkbox
+              id="featured"
+              checked={localFilters.featured || false}
+              onCheckedChange={(value) => handleChange("featured", value)}
+            />
+            <Label htmlFor="featured">Destacado</Label>
+          </div>
+        </>
+      )}
 
       <Button variant="destructive" onClick={clearFilters}>Limpiar Filtros</Button>
     </div>
