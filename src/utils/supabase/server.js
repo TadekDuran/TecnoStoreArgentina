3b80d54bac3708fdae5d564a5fca7c3aab1d2e6c
TadekDuran <tadekduran@gmail.com>
2025-04-08 13:50:40 -0300

    update(supabase server client): now the server client uses the Supabase Service Key

--- src/utils/supabase/server.js
+++ src/utils/supabase/server.js
@@ -6,7 +6,7 @@ import { createServerClient } from '@supabase/ssr'
 
     return createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      process.env.SUPABASE_SERVICE_KEY,
       {
         cookies: {
           getAll() {
@@ -18,9 +18,6 @@ import { createServerClient } from '@supabase/ssr'
                 cookieStore.set(name, value, options)
               )
             } catch {
-              // The `setAll` method was called from a Server Component.
-              // This can be ignored if you have middleware refreshing
-              // user sessions.
             }
           },
         },
