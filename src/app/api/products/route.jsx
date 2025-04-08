ad2416419c8aba2f0ac22a00be104513f4998465
TadekDuran <tadekduran@gmail.com>
2025-04-08 13:55:50 -0300

    update: routes for GET requests in products now import the browserClient version of the supabase client, that uses the public ANON_KEY

--- src/app/api/products/route.jsx
+++ src/app/api/products/route.jsx
@@ -1,4 +1,4 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
