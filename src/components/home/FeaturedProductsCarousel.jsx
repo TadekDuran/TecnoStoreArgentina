3efe999424a3332cd20550ab7fde5072f242a98f
TadekDuran <tadekduran@gmail.com>
2025-04-14 16:42:32 -0300

    update(FeaturedProductsCarousel): now carousel is centered when there are few items so it doesn't look asymmetric

--- src/components/home/FeaturedProductsCarousel.jsx
+++ src/components/home/FeaturedProductsCarousel.jsx
@@ -9,27 +9,37 @@ import {
 import ProductCard from "@/components/catalog/ProductCard";
 
 const FeaturedProductsCarousel = ({ data }) => {
+  const fewItems = data.length < 4;
+
   return (
     <div className="flex justify-center px-4">
       <Carousel
         className="w-full max-w-[75vw]"
         opts={{
-          align: "start",
           containScroll: "trimSnaps",
         }}
       >
-        <CarouselContent className="-ml-4">
+        <CarouselContent
+          className={`-ml-4 ${fewItems ? "lg:justify-center" : ""}`}
+        >
           {data.map((product) => (
             <CarouselItem
               key={product.id}
-              className="basis-full pl-4 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+              className={`basis-full pl-4 sm:basis-1/2 md:basis-1/3 ${fewItems ? "lg:basis-auto" : "lg:basis-1/4"}`}
             >
-              <ProductCard product={product} />
+              <div className="mx-auto w-full max-w-sm">
+                <ProductCard product={product} />
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+
+        {data.length > 1 && (
+          <>
+            <CarouselPrevious />
+            <CarouselNext />
+          </>
+        )}
       </Carousel>
     </div>
   );
