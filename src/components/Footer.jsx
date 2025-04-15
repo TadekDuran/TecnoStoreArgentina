2b8d2b683ede78467cc6151733315c01792460e6
TadekDuran <tadekduran@gmail.com>
2025-04-15 13:26:27 -0300

    feat(Footer): create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center border-t border-white/10 p-4">
-      <p>Footer</p>
+    <footer className="flex flex-col items-center border-t border-white/10 bg-secondary-background p-4 text-primary-text">
+      <h1>Buenos Aires, Argentina</h1>
+      <Link
+        href="/contact"
+        className="flex items-center gap-2 text-xl font-bold underline"
+      >
+        Contacto
+        <ExternalLink size={15} />
+      </Link>
+
+      <p className="mt-1 text-xs">
+        Desarrollado por{" "}
+        <a
+          href="https://github.com/TadekDuran/"
+          target="_blank"
+          className="underline"
+        >
+          Tadek Duran
+        </a>
+      </p>
     </footer>
   );
 };
