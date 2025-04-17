b0b9c9b93f24d12e4e2ab5c343fc640560a1ffb3
TadekDuran <tadekduran@gmail.com>
2025-04-17 15:34:57 -0300

    eliminate supressHydrationWarning

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -56,7 +56,7 @@ const Admin = () => {
   }
 
   return (
-    <div suppressHydrationWarning className="m-2 flex flex-col gap-2">
+    <div className="m-2 flex flex-col gap-2">
       <h1>Panel Administrador</h1>
       <CreateProductForm />
       <Button
