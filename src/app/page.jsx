6016b802580f4c088f9033e58455336140530d17
Tadek <tadekduran@gmail.com>
2025-05-19 20:27:49 -0300

    feat(Home): create FechuSponsorVideos component and implement it

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import FeaturedProductsSection from "@/components/home/FeaturedProductsSection";
 import SuprapixelSponsorVideos from "@/components/home/SuprapixelSponsorVideos";
+import FechuSponsorVideos from "@/components/home/FechuSponsorVideos";
 
 export default function Home() {
-  const queries = {
-    featured: true,
-    page: 1,
-    limit: 10,
-    sortBy: "price",
-    order: "desc",
-  };
-  const { data, loading, error, getProducts } = useProducts();
-
-  useEffect(() => {
-    getProducts(queries);
-  }, []);
-
-  return (
+    return (
     <div className="flex min-h-screen flex-col items-center bg-primary-background py-4 sm:py-6 lg:py-8">
       <FeaturedProductsSection />
 
 
       <SuprapixelSponsorVideos />
+
+      <FechuSponsorVideos />
     </div>
   );
 }
