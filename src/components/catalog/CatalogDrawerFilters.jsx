70279de132180fbbafc3d5950175779cb946fc5c
TadekDuran <tadekduran@gmail.com>
2025-05-22 14:53:54 -0300

    update(CatalogDrawerFilters): improve filter visibility and accesibility

--- src/components/catalog/CatalogDrawerFilters.jsx
+++ src/components/catalog/CatalogDrawerFilters.jsx
@@ -6,7 +6,7 @@ import {
   DrawerTrigger,
   DrawerHeader,
   DrawerTitle,
-  DrawerDescription
+  DrawerDescription,
 } from "@/components/ui/drawer";
 import {
   Select,
@@ -22,10 +22,10 @@ const CatalogDrawerFilters = ({
   selectValue,
   queries,
   setQueries,
-  brandList
+  brandList,
 }) => {
   return (
-    <div className="mb-6 w-full md:hidden">
+    <div className="mb-6 w-4/5 md:hidden">
       <Drawer>
         <DrawerTrigger asChild>
           <button className="w-full rounded-md bg-secondary-background p-3 text-center font-semibold text-primary-text shadow">
@@ -33,17 +33,17 @@ const CatalogDrawerFilters = ({
           </button>
         </DrawerTrigger>
 
-        <DrawerContent className="bg-secondary-background p-6">
+        <DrawerContent className="flex max-h-dvh flex-col bg-secondary-background px-4 pb-20">
           <DrawerHeader className="hidden">
             <DrawerTitle>Filtro de productos</DrawerTitle>
-            <DrawerDescription>Menú para aplicar filtros a los productos</DrawerDescription>
+            <DrawerDescription>
+              Menú para aplicar filtros a los productos
+            </DrawerDescription>
           </DrawerHeader>
-          <div className="mb-4 text-primary-text">
-            <label className="mr-2" htmlFor="sort">
-              Ordenar por:
-            </label>
+          <div className="flex flex-col gap-2 text-primary-text">
+            <label htmlFor="sort">Ordenar por:</label>
             <Select onValueChange={handleSortChange} value={selectValue}>
-              <SelectTrigger className="w-full">
+              <SelectTrigger>
                 <SelectValue placeholder="Selecciona orden" />
               </SelectTrigger>
               <SelectContent>
@@ -53,11 +53,15 @@ const CatalogDrawerFilters = ({
             </Select>
           </div>
 
-          <ProductsFilter queries={queries} setQueries={setQueries} brandList={brandList} />
+          <ProductsFilter
+            queries={queries}
+            setQueries={setQueries}
+            brandList={brandList}
+          />
 
           <DrawerClose asChild>
-            <button className="mt-6 w-full rounded-md bg-red-600 p-2 font-semibold text-primary-text hover:bg-red-700">
-              Cerrar
+            <button className="mb-2 w-4/5 place-self-center rounded-md bg-tertiary-background p-2 font-semibold text-primary-text">
+              Aplicar Filtros
             </button>
           </DrawerClose>
         </DrawerContent>
