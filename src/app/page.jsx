4a1be12490dd3e8ad52634bcb1e57384f206b522
TadekDuran <tadekduran@gmail.com>
2025-05-31 15:59:40 -0300

    feat(Home): implement Hero component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionFadeWrapper from "@/components/home/SectionFadeWrapper";
+import Hero from "@/components/home/Hero";
 import FeaturedProductsSection from "@/components/home/FeaturedProductsSection";
 import FeaturedBrands from "@/components/home/FeaturedBrands";
 import SuprapixelSponsorVideos from "@/components/home/SuprapixelSponsorVideos";
@@ -8,6 +9,10 @@ import FechuSponsorVideos from "@/components/home/FechuSponsorVideos";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center bg-primary-background">
+      <SectionFadeWrapper>
+        <Hero />
+      </SectionFadeWrapper>
+
       <SectionFadeWrapper>
         <FeaturedProductsSection />
       </SectionFadeWrapper>
