13170af1739c3193a2433a49e5b24d33f452c06d
TadekDuran <tadekduran@gmail.com>
2025-08-12 15:44:20 -0300

    update(Navbar): remove buttons from desktop version of the navbar to make it look cleaner. Add Orlando Market button

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -14,8 +14,9 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
+import orlando_logo from "@/assets/Orlando-Logotipo.webp";
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+import { Menu, ExternalLink } from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -23,36 +24,42 @@ const Navbar = () => {
       {/* Desktop */}
       <NavigationMenuList className="mx-auto hidden w-screen items-center justify-center gap-x-8 bg-secondary-background px-8 py-4 text-primary-text md:flex">
         <NavigationMenuItem>
-          <Button
-            asChild
-            className="bg-tertiary-background px-8 py-6 text-lg text-button-text hover:bg-tertiary-background-hover"
-          >
-            <Link href="/">Inicio</Link>
-          </Button>
+          <Link href="/">
+            <h1 className="px-4 py-2 text-2xl font-medium text-button-text hover:underline">
+              Home
+            </h1>
+          </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Button
-            asChild
-            className="bg-tertiary-background px-8 py-6 text-lg text-button-text hover:bg-tertiary-background-hover"
-          >
-            <Link href="/catalog">Catálogo</Link>
-          </Button>
+          <Link href="/catalog">
+            <h1 className="px-4 py-2 text-2xl font-medium text-button-text hover:underline">
+              Catálogo
+            </h1>
+          </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Button
-            asChild
-            className="bg-tertiary-background px-8 py-6 text-lg text-button-text hover:bg-tertiary-background-hover"
-          >
-            <Link href="/faq">Preguntas Frecuentes</Link>
-          </Button>
+          <Link href="/faq">
+            <h1 className="px-4 py-2 text-2xl font-medium text-button-text hover:underline">
+              Preguntas Frecuentes
+            </h1>
+          </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Button
-            asChild
-            className="bg-tertiary-background px-8 py-6 text-lg text-button-text hover:bg-tertiary-background-hover"
+          <Link href="/contact">
+            <h1 className="px-4 py-2 text-2xl font-medium text-button-text hover:underline">
+              Contacto
+            </h1>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link
+            href="https://orlando-market.com/"
+            target="_blank"
+            className="flex items-center gap-2 rounded-sm border border-slate-400 bg-gray-600 px-2 py-1 duration-150 hover:bg-slate-400"
           >
-            <Link href="/contact">Contacto</Link>
-          </Button>
+            <img src={orlando_logo.src} alt="Orlando Logotipo" />{" "}
+            <ExternalLink className="text-gray-300" />
+          </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
 
@@ -71,12 +78,17 @@ const Navbar = () => {
             </SheetHeader>
             <div className="mt-6 flex flex-col gap-6">
               {["/", "/catalog", "/faq", "/contact"].map((href, i) => {
-                const labels = ["Inicio", "Catálogo", "Preguntas Frecuentes", "Contacto"];
+                const labels = [
+                  "Inicio",
+                  "Catálogo",
+                  "Preguntas Frecuentes",
+                  "Contacto",
+                ];
                 return (
                   <Button
                     key={i}
                     asChild
-                    className="w-full text-lg bg-tertiary-background py-6 text-button-text hover:bg-tertiary-background-hover"
+                    className="w-full bg-tertiary-background py-6 text-lg text-button-text hover:bg-tertiary-background-hover"
                   >
                     <SheetClose asChild>
                       <Link href={href}>{labels[i]}</Link>
@@ -84,6 +96,13 @@ const Navbar = () => {
                   </Button>
                 );
               })}
+              <Link
+                href="https://orlando-market.com/"
+                target="_blank"
+                className="flex items-center gap-2 rounded-sm border border-slate-400 bg-gray-600 py-3 duration-150 hover:bg-slate-400"
+              >
+                <img src={orlando_logo.src} alt="Orlando Logotipo" />
+              </Link>
             </div>
           </SheetContent>
         </Sheet>
